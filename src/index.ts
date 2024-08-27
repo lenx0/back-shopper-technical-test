e6507bcfed7498659d997819fc905c06d7036070
thiago <thiagofreitas1106@gmail.com>
2024-08-27 13:01:13 -0300

    mongoose config done

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
 
-const app = express();
+mongoose
+  .connect(
+    "mongodb+srv://mongodefault:Px2KIGnVtCsgQeEz@cluster0.ahjgu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    const app = express();
+    const port = 3001;
 
-const port = 3001;
-
-app.listen(3001, () => {
-  console.log(`Server is running on port ${port}`);
-});
+    app.listen(3001, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch(() => console.log("the mongoDB connection is failed"));
